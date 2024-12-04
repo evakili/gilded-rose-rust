efb75b33544b52af582200ef47c32a09f8df5865
Esmaeil Vakili <evakili@users.noreply.github.com>
2024-12-04 10:40:31 +0330

    refactor: revert if condition

--- src/lib.rs
+++ src/lib.rs
@@ -32,14 +32,8 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for i in 0..self.items.len() {
-            if self.items[i].name != "Aged Brie" && self.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+            if self.items[i].name == "Aged Brie" || self.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
             {
-                if self.items[i].quality > 0 {
-                    if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        self.items[i].quality = self.items[i].quality - 1;
-                    }
-                }
-            } else {
                 if self.items[i].quality < 50 {
                     self.items[i].quality = self.items[i].quality + 1;
 
@@ -55,6 +49,12 @@ impl GildedRose {
                                 self.items[i].quality = self.items[i].quality + 1;
                             }
                         }
+                    }
+                }
+            } else {
+                if self.items[i].quality > 0 {
+                    if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
+                        self.items[i].quality = self.items[i].quality - 1;
                     }
                 }
             }
